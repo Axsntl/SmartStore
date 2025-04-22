d14cec9fea95ff7a4d2acf1ea909481bebccbdd0
axsntl <axsntl3@gmail.com>
2025-04-21 23:22:37 -0600

    Agregado boton de regreso en sell

--- screens/productupload.py
+++ screens/productupload.py
@@ -118,6 +118,7 @@ def ProductUploadView(page: ft.Page):
                             ft.ElevatedButton("Seleccionar video", on_click=pick_video, icon=ft.icons.VIDEO_FILE),
                         ]),
                         ft.ElevatedButton("Publicar producto", on_click=publicar_producto, icon=ft.icons.UPLOAD),
+                        ft.ElevatedButton("Volver", on_click=lambda _: page.go_back(), icon=ft.icons.ARROW_BACK),  # Botón para retroceder
                     ],
                     spacing=15,
                 ),
